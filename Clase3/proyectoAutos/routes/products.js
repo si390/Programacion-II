4bd0e43d9648ff6d3abb7275fa09c2fca7fe94f1
si390 <soyolaiberico@udesa.edu.ar>
2024-03-20 18:27:43 -0300

    autos

---
+++ Clase3/proyectoAutos/routes/products.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const datosAutos = require('../db/index');
+router.get('/products', function (req, res) {
+    return res.send(datosAutos);
+});
+
+router.get('/products/brand', function (req,res) {
+    let marcaProducto = req.params.id;
+});
+
+router.get('/products/color', function (req,res) {
+    let colorProducto = req.params.id;
+});
+
+router.get('/products/modelo', function (req,res) {
+    let modeloProducto = req.params.id;
+});
+
+router.get('/products/year', function (req,res) {
+    let yearProducto = req.params.id;
+});
+    
+
+module.exports = router;
