d434cab44a6cb0de80c21942c5f9330795214ca0
si390 <soyolaiberico@udesa.edu.ar>
2024-03-27 11:28:42 -0300

    routes

--- Book Web Development/hello.js
+++ Book Web Development/hello.js
@@ -1,20 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 const port = process.env.PORT || 3000;
+
+function serveStaticFile(res, path, contentType, responseCode = 200){
+    fs.readFile(__dirname + path, (err, data) => {
+        if(err){
+            res.writeHead(500, { 'Content-Type': 'text/plain' })
+            return res.end('500 - Internal Error')
+        }
+        res.writeHead(responseCode, { 'Content-Type': contentType})
+        res.end(data)
+    })
+}
 
 const server = http.createServer((req, res) => {
   const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
   switch(path) {
     case '':
-      res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end('Homepage')
+        serveStaticFile(res, './public/home.html', 'text/html')
       break
     case '/about':
-      res.writeHead(200, { 'Content-Type': 'text/plain' })
-      res.end('About')
+        serveStaticFile(res, './public/about.html', 'text/html')
+      break
+    case '/img/logo.png':
+        serveStaticFile(res, './public/img/logo.png', 'img/png')
       break
     default:
-      res.writeHead(404, { 'Content-Type': 'text/plain' })
-      res.end('Not Found')
+        serveStaticFile(res, './public/404.html', 'text/html', 404)
       break
 
 }})
